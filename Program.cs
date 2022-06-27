bd1636789c9e1a2c46092fa33a1f4e8bb0a957bb
Abdulhai-Mohamed <78891145+Abdulhai-Mohamed@users.noreply.github.com>
2022-06-27 17:29:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// C# program to illustrate the
+// Capacity Property of List<T>
+using System;
+using System.Collections.Generic;
+
+class Geeks
+{
+
+	public static void Main(String[] args)
+	{
+
+		 //List<string> list_strings = new List<string>();
+         int ii=0;
+        Console.WriteLine(5);
+        /*
+		while (ii<3)
+        {
+            Console.WriteLine(ii);
+            int counter=0 ;
+            String st = Console.ReadLine();
+            if (st == null)
+                     {
+            break;
+                   }
+                   else
+                   {
+            list_strings.Add(st);
+            counter += 1;
+            }
+        }
+        */
+        //foreach(string i in list_strings)
+        //      {
+        //	Console.WriteLine(i);
+        //      }
+        //Console.WriteLine(st);
+
+
+    }
+}
